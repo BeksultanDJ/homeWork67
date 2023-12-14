e53cd2e33f115f9fc117566bd8a186fe55fadce9
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-14 18:29:05 +0600

    сделал отобрадение клавиатуры и указал интерфейс для кнопок

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,30 @@ const AccessMessage: React.FC<AccessMessageProps> = ({ message, color }) => {
     );
 };
 
+interface KeyboardProps {
+    onButtonClick: (digit: string) => void;
+    onClear: () => void;
+    onEnter: () => void;
+}
+
+const Keyboard: React.FC<KeyboardProps> = ({ onButtonClick, onClear, onEnter }) => {
+    const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    return (
+        <div className="keyboard">
+            {digits.map((digit) => (
+                <button className="btns" key={digit} onClick={() => onButtonClick(digit.toString())}>
+                    {digit}
+                </button>
+            ))}
+            <button className="btns" onClick={onClear}>{"<"}</button>
+            <button className="btns" onClick={onEnter}>E</button>
+        </div>
+    );
+};
+
+
+
 const App = () => {
     return (
         <Provider store={store}>
