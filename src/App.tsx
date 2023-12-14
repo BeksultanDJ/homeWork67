f753693dc5835ba0d3320ecbd7520b26a2d47607
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-14 18:12:55 +0600

    Создание хранилища Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+
+import { createStore } from 'redux';
 import './App.css';
 
-function App() {
+const SET_PIN = 'SET_PIN';
+const CLEAR_PIN = 'CLEAR_PIN';
 
-  return (
-    <>
-      
+const setPin = (digit: string) => {
+    return { type: SET_PIN, payload: digit };
+};
 
-    </>
-  )
-}
+const clearPin = () => {
+    return { type: CLEAR_PIN };
+};
 
-export default App
+const pinReducer = (state = '', action: any) => {
+    switch (action.type) {
+        case SET_PIN:
+            return state.length < 4 ? state + action.payload : state;
+        case CLEAR_PIN:
+            return '';
+        default:
+            return state;
+    }
+};
+
+const store = createStore(pinReducer);
